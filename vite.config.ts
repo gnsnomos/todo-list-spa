c51edbbc883c3fc9cf3a98c5805d8c1ff4ee07e1
giannis.sarantopoulos <giannis.sarantopoulos@hellasdirect.gr>
2026-09-07 00:46:18 +0300

    update base value

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   // Άλλαξε το "/shopping-list-app/" ώστε να ταιριάζει ΑΚΡΙΒΩΣ με το όνομα
   // του GitHub repository σου (π.χ. αν το repo λέγεται "psonia",
   // βάλε base: "/psonia/"). Αν χρησιμοποιείς custom domain, βάλε base: "/".
-  base: "/shopping-list-app/",
+  base: "/todo-list-spa/",
 });
